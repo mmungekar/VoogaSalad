274ab4d18f0219551e38d07e122c0fddc9d2ae0d
Elliott Bolzan <elliott.bolzan@duke.edu>
2017-03-28 18:24:21 -0400

    Created package structure.

---
+++ src/starter/Main.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package starter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Elliott Bolzan
+ *
+ * This class is used to launch the program. 
+ */
+public class Main extends Application {
+	
+	/**
+	 * Begins the launching process.
+	 * @param args the arguments passed into main. 
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * Launches the user interface.
+	 * @param primaryStage the application's primary stage / window.
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Jay, this is where you create an instance of your starter front end, using primaryStage.
+		// I recommend to keep that starter UI code in this starter package!
+	}
+
+}
