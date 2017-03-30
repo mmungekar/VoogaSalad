4082b905861e02071c0e9782a7050ac4c704db5f
Jesse Yue <someonebob156@gmail.com>
2017-03-30 17:14:17 -0400

    Added Player Use Case Implementation

---
+++ src/usecases/ExitAGame.java
@@ -0,0 +1,21 @@
+package usecases;
+
+import javafx.scene.control.Button;
+import player.Menu;
+
+/**
+ * 
+ * @author Jesse
+ *
+ *	This is an implementation of the use case for exiting a game
+ */
+public class ExitAGame {
+	
+	public ExitAGame(Menu mainmenu){
+		//Button is created for exiting
+		Button exit = new Button("Exit");
+		//Upon clicking, user is taken back to the player main menu
+		exit.setOnAction(e -> mainmenu.display());
+	}
+
+}
