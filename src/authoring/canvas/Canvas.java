4b6f02f1ccb9dfd333e1fdfec016fab7ef053f17
Jimmy Shackford <jas199@duke.edu>
2017-04-01 16:56:54 -0400

    Made nodes draggable and snap to grid.

--- src/authoring/canvas/Canvas.java
+++ src/authoring/canvas/Canvas.java
@@ -3,6 +3,7 @@ package authoring.canvas;
 import authoring.Workspace;
 import authoring.views.View;
 import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
@@ -24,6 +25,7 @@ public class Canvas extends View
 	private Workspace workspace;
 	private final int TILE_SIZE = 25;
 
+	private ScrollPane scrollScreen;
 	private Pane layer;
 	private int width;
 	private int height;
@@ -40,20 +42,59 @@ public class Canvas extends View
 	private void setup()
 	{
 		setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-		ScrollPane scrollScreen = new ScrollPane();
+		scrollScreen = new ScrollPane();
 		layer = new Pane();
 		layer.setPrefHeight(height);
 		layer.setPrefWidth(width);
 		layer.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-		Rectangle add = new Rectangle();
-		add.setHeight(100);
-		add.setWidth(100);
-		add.setFill(Color.RED);
-		layer.getChildren().add(add);
+
+		Rectangle rect = new Rectangle();
+		rect.setWidth(100);
+		rect.setHeight(100);
+		rect.setFill(Color.CORAL);
+		makeDraggable(rect);
+		layer.getChildren().add(rect);
+
 		scrollScreen.setContent(layer);
-//		setupGrid();
+		setupGrid();
 		this.setCenter(scrollScreen);
 	}
 
+	private void setupGrid()
+	{
+		for (int i = 0; i < width / TILE_SIZE; i++) {
+			for (int j = 0; j < height / TILE_SIZE; j++) {
+				Circle gridMarker = new Circle();
+				gridMarker.setCenterX(i * TILE_SIZE);
+				gridMarker.setCenterY(j * TILE_SIZE);
+				gridMarker.setRadius(1);
+				gridMarker.setFill(Color.GREY);
+				layer.getChildren().add(gridMarker);
+			}
+		}
+	}
+
+	private void makeDraggable(Node node)
+	{
+		node.setOnMouseDragged(new EventHandler<MouseEvent>()
+		{
+
+			@Override
+			public void handle(MouseEvent event)
+			{
+				Bounds scrollViewportBounds = scrollScreen.getViewportBounds();
+				double settingsWidth = workspace.getPane().getChildrenUnmodifiable().get(0).getBoundsInParent()
+						.getWidth();
+				double horizontalScrollAmount = scrollScreen.getHvalue() * (width - scrollViewportBounds.getMaxX());
+				double verticalScrollAmount = scrollScreen.getVvalue() * (height - scrollViewportBounds.getMaxY());
+				node.setTranslateX(
+						((int) (event.getSceneX() - settingsWidth + horizontalScrollAmount) / TILE_SIZE) * TILE_SIZE);
+				node.setTranslateY(((int) (event.getSceneY() + verticalScrollAmount) / TILE_SIZE) * TILE_SIZE);
+				System.out.println(horizontalScrollAmount);
+				System.out.println(verticalScrollAmount);
+			}
+
+		});
+	}
 
 }
