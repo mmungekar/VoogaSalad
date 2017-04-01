19f152ac4d5b16b935760f93af6430467d98ecde
Jimmy Shackford <jas199@duke.edu>
2017-04-01 15:20:58 -0400

    Added scrolling canvas

--- src/authoring/canvas/Canvas.java
+++ src/authoring/canvas/Canvas.java
@@ -3,25 +3,52 @@ package authoring.canvas;
 import authoring.Workspace;
 import authoring.views.View;
 import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class Canvas extends View {
-	
-	// I recommend having a tabbed structure in here, to account for multiple levels.
+public class Canvas extends View
+{
+
+	// I recommend having a tabbed structure in here, to account for multiple
+	// levels.
 
 	private Workspace workspace;
-	
-	public Canvas(Workspace workspace) {
+	private final int TILE_SIZE = 25;
+
+	private Pane layer;
+	private int width;
+	private int height;
+
+	public Canvas(Workspace workspace)
+	{
 		super(workspace.getResources().getString("CanvasTitle"));
+		height = 1000;
+		width = 1000;
 		this.workspace = workspace;
 		setup();
 	}
-	
-	private void setup() {
+
+	private void setup()
+	{
 		setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		ScrollPane scrollScreen = new ScrollPane();
+		layer = new Pane();
+		layer.setPrefHeight(height);
+		layer.setPrefWidth(width);
+		layer.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		Rectangle add = new Rectangle();
+		add.setHeight(100);
+		add.setWidth(100);
+		add.setFill(Color.RED);
+		layer.getChildren().add(add);
+		scrollScreen.setContent(layer);
+		// setupGrid();
+		this.setCenter(scrollScreen);
 	}
 
 }
