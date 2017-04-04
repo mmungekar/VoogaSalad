07ecd2df498bc4b660b5c6740e944f3401065a2a
Mina Mungekar <mina.mungekar@duke.edu>
2017-04-04 00:20:17 -0400

    Filling in necessary methods. Pertains to issue #12

--- src/authoring/Workspace.java
+++ src/authoring/Workspace.java
@@ -3,6 +3,8 @@
  */
 package authoring;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import authoring.canvas.Canvas;
@@ -11,6 +13,7 @@ import authoring.settings.Settings;
 import authoring.utils.Factory;
 import authoring.views.View;
 import discussion.Discussion;
+import engine.game.Level;
 import javafx.geometry.Insets;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.SplitPane;
@@ -18,6 +21,7 @@ import javafx.scene.layout.BorderPane;
 
 /**
  * @author Elliott Bolzan
+ * Modified by Mina Mungekar
  *
  */
 public class Workspace extends View {
@@ -41,12 +45,12 @@ public class Workspace extends View {
 	 */
 	private void setup() {
 		pane = new SplitPane();
-		//settings = new Settings(this, 0);
 		panel = new Panel(this, 0);
 		canvas = new Canvas(this);
 		pane.getItems().addAll(panel,canvas);
-		pane.setDividerPositions(0.3, 0.75);
-		setPadding(new Insets(4));
+		pane.setDividerPositions(Double.parseDouble(resources.getString("DividerPositionX")),
+				Double.parseDouble(resources.getString("DividerPositionY")));
+		setPadding(new Insets(Integer.parseInt(resources.getString("WorkSpaceInsets"))));
 		setCenter(pane);
 	}
 
@@ -61,6 +65,11 @@ public class Workspace extends View {
 	public void showMessage(String message) {
 		Factory factory = new Factory(resources);
 		factory.makeAlert(AlertType.ERROR, "ErrorTitle", "ErrorHeader", message).showAndWait();
+	}
+	
+	public List getEntities(){
+		//return canvas's entities (i.e. canvas.getLevel())
+		return new ArrayList<>();
 	}
 
 }
--- src/authoring/settings/Settings.java
+++ src/authoring/settings/Settings.java
@@ -9,6 +9,8 @@ import authoring.Workspace;
 import authoring.utils.Direction;
 import authoring.views.CollapsibleView;
 import authoring.views.View;
+import engine.Entity;
+import engine.game.Level;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -41,11 +43,15 @@ public class Settings extends View {
 	}
 	
 	private void dummyMethod(){
-		int i = 0;
+		/*Level level = new Level();
+		for (Object object: workspace.getEntities()){
+			level.addEntity((Entity)object);
+		}
+		Insert code to send to game data*/
+		
 	}
 	
-	private void chooseFile(Consumer<File> r)
-	{
+	private void chooseFile(Consumer<File> r){
 		FileChooser fileChooser = new FileChooser();
 				File file = fileChooser.showOpenDialog(new Stage());
 				if (file != null){
