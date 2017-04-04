8bfd8f59fa079e692bede1610d85db524d35c0ab
Mina Mungekar <mina.mungekar@duke.edu>
2017-04-03 23:17:19 -0400

    reconfigure the authoring structure

--- src/authoring/settings/Settings.java
+++ src/authoring/settings/Settings.java
@@ -12,22 +12,22 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 /**
  * 
  * @author Mina
  *
  */
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
-public class Settings extends CollapsibleView {
+public class Settings extends View {
 
 	private Workspace workspace;
 	private VBox settingsContainer;
 	
-	public Settings(Workspace workspace, int index) {
-		super(workspace, workspace.getPane(), workspace.getResources().getString("SettingsTitle"), index, Direction.LEFT, true);
+	public Settings(Workspace workspace) {
+		super(workspace.getResources().getString("SettingsTitle"));
 		this.workspace = workspace;
 		configureSettings();
 	}
