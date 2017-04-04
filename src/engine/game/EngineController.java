c3f5810f2da4a82a3d7c6aa4b265b421018d090a
Nikita Zemlevskiy <naz7@duke.edu>
2017-04-04 17:39:33 -0400

    implement methods in EngineController. add ClassFinder (identical to authoring)

--- src/engine/game/EngineController.java
+++ src/engine/game/EngineController.java
@@ -1,20 +1,51 @@
 package engine.game;
 
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.stream.Collectors;
+import engine.Event;
+import engine.Action;
+
 public class EngineController {
 
-	public List<String> getAllActions(){
-		
+	private ClassFinder finder;
+
+	public EngineController() {
+		finder = new ClassFinder();
 	}
-	public List<String> getAllEvents{
-		
+
+	public List<String> getAllActions() {
+		return findClasses("engine.actions");
 	}
-	public Entity createEntity(String entity){
-		
+
+	public List<String> getAllEvents() {
+		return findClasses("engine.events");
 	}
-	public Action createAction(String action){
-		
+
+	public Event createEvent(String event) {
+		try {
+			return (Event) getInstance("engine.events." + event + "Event");
+		} catch (Exception e) {
+			return null;
+		}
 	}
-	public Event createAction(){
-		
+
+	public Action createAction(String action) {
+		try {
+			return (Action) getInstance("engine.actions." + action + "Event");
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private List<String> findClasses(String path) {
+		List<Class<?>> classes = finder.find(path);
+		return classes.stream().map(s -> s.toString()).collect(Collectors.toList());
+	}
+
+	private Object getInstance(String path) throws Exception {
+		Class<?> clazz = Class.forName(path);
+		Constructor<?> ctor = clazz.getDeclaredConstructor();
+		return ctor.newInstance();
 	}
 }
