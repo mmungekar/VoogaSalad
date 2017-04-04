fc29396490775acb8b17de4f7e10513fe5690d90
Nikita Zemlevskiy <naz7@duke.edu>
2017-04-04 16:44:18 -0400

    add properties files. close issue #27

--- src/engine/ActionInterface.java
+++ src/engine/ActionInterface.java
@@ -6,7 +6,9 @@ public interface ActionInterface {
 	//external
 	Map<String, Object> getParams(); // will use reflection
 	void setParams(Map<String, Object> params); // will use reflection
-
+	String getDisplayName();
+	String getDisplayDescription();
+	
 	//internal
 	void act(); // carry out the action triggered by the event. Will need a reference to game
 }
--- src/engine/Event.java
+++ src/engine/Event.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
+
+import engine.game.eventobserver.EventObservable;
 
 public abstract class Event implements EventInterface {
 	
 	private List<Action> actions;
 	private Map<String, Object> params;
+	private ResourceBundle resources;
+	private EventObservable observable;
 
 	public Event(){
 		actions = new ArrayList<Action>();
 		params = new HashMap<String, Object>();
+		resources = ResourceBundle.getBundle("Events");
 	}
 	
 	@Override
@@ -36,5 +42,18 @@ public abstract class Event implements EventInterface {
 	protected void trigger(){
 		for (Action action: actions)
 			action.act();
+	}
+	@Override
+	public String getDisplayName() {
+		return resources.getString(getClass().toString());
+	}
+
+	@Override
+	public String getDisplayDescription() {
+		return resources.getString(getClass() + "Description");
+	}
+	
+	public void addEventObservable(EventObservable observable){
+		this.observable = observable;
 	}
 }
