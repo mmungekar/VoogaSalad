3b2385bbc2eebf3d9bd107a6ba351553b9007f29
Matthew Barbano <matthew.barbano@duke.edu>
2017-04-08 01:48:59 -0400

    And another uncomment

--- src/starter/StartMenu.java
+++ src/starter/StartMenu.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javax.swing.ImageIcon;
-//import com.apple.eawt.Application;
+import com.apple.eawt.Application;
 
 import authoring.AuthoringEnvironment;
 import javafx.event.ActionEvent;
