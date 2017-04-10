2ab771a72d6ffe3d0a144693e10706ad1ba600e4
Elliott Bolzan <elliott.bolzan@duke.edu>
2017-04-09 21:11:29 -0400

    Created Game class.

--- src/engine/events/KeyPressEvent.java
+++ src/engine/events/KeyPressEvent.java
@@ -6,7 +6,7 @@ import engine.game.eventobserver.InputObservable;
 import javafx.scene.input.KeyCode;
 
 public class KeyPressEvent extends Event {
-
+	
 	public KeyPressEvent(KeyCode keyToListenFor) {
 		addParam(new Parameter("Key", KeyCode.class, keyToListenFor));
 	}
