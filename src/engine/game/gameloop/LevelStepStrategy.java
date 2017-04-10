73e0dad05b491901883c1036abed5ce042f7ca2b
Kyle Finke <Kyle Finke>
2017-04-09 20:51:23 -0400

    Added events and actions to resource files

--- src/engine/game/gameloop/LevelStepStrategy.java
+++ src/engine/game/gameloop/LevelStepStrategy.java
@@ -10,7 +10,7 @@ import engine.actions.JumpAction;
 import engine.actions.MoveAction;
 import engine.actions.ShiftHorizontalAction;
 import engine.actions.WalkAction;
-import engine.actions.ZeroDownSpeedAction;
+import engine.actions.ZeroVerticalSpeedAction;
 import engine.actions.ZeroHorizontalSpeedAction;
 import engine.entities.BlockEntity;
 import engine.entities.CharacterEntity;
@@ -175,7 +175,7 @@ public class LevelStepStrategy implements StepStrategy{
 		CollisionEvent groundCollision = new CollisionEvent();
 		groundCollision.setCollision(new Collision(mario,block, CollisionSide.TOP));
 		block.addEvent(groundCollision);
-		groundCollision.addAction(new ZeroDownSpeedAction(mario));
+		groundCollision.addAction(new ZeroVerticalSpeedAction(mario));
 		
 		levelManager.getCurrentLevel().getEntities().add(mario);
 		levelManager.getCurrentLevel().getEntities().add(block);
