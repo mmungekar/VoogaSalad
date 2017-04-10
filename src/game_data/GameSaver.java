62a637e78a28d616bd9389e0e8ca81de742eb633
Jimmy Shackford <jas199@duke.edu>
2017-04-10 19:45:51 -0400

    Added authoring supportability for saving. LayerEditor and LevelEditor can convert entities to a Level object.

--- src/game_data/GameSaver.java
+++ src/game_data/GameSaver.java
@@ -1,15 +1,14 @@
 package game_data;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
@@ -17,24 +16,24 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import engine.Entity;
 import engine.game.Level;
-import static java.nio.file.StandardCopyOption.*;
 
-public class GameSaver {
+public class GameSaver
+{
 
-
-	
-	
-	public void saveGame(Game game, String filepath){
-	
-	}
-	//filepath provides the specific directory where the game will be stored
-	public void saveGame(List<Level> levels,String filepath){
-		savelevels(levels,filepath);
-		
-
+	public void saveGame(Game game, String filepath)
+	{
+		this.saveGame(game.getLevels(), filepath);
 	}
 
-	private void savelevels(List<Level> levels, String filepath) {
+	// filepath provides the specific directory where the game will be stored
+	public void saveGame(List<Level> levels, String filepath)
+	{
+		savelevels(levels, filepath);
+
+	}
+
+	private void savelevels(List<Level> levels, String filepath)
+	{
 		File entityfolder = new File(filepath + "/levels");
 		if (!entityfolder.exists()) {
 			entityfolder.mkdirs();
@@ -44,6 +43,7 @@ public class GameSaver {
 		for (int i = 0; i < levels.size(); i++) {
 
 			List<Entity> entities = new ArrayList<Entity>(levels.get(i).getEntities());
+			System.out.println(levels.get(i).getEntities());
 			List<String> entityfilepaths = new ArrayList<String>();
 
 			for (int j = 0; j < entities.size(); j++) {
@@ -57,19 +57,18 @@ public class GameSaver {
 		}
 	}
 
-	
-	
-	public String saveEntity(Entity entity, String filepath){
-		File entityfolder=new File(filepath+"/entities");
-		if(!entityfolder.exists()){
+	public String saveEntity(Entity entity, String filepath)
+	{
+		File entityfolder = new File(filepath + "/entities");
+		if (!entityfolder.exists()) {
 			entityfolder.mkdirs();
 		}
-		String entityfilepath="";
-		try{
-			saveEntityImage(entity,filepath);
-			entityfilepath=filepath+"/entities/"+entity.getName()+".xml";
+		String entityfilepath = "";
+		try {
+			saveEntityImage(entity, filepath);
+			entityfilepath = filepath + "/entities/" + entity.getName() + ".xml";
 			File entityfile = new File(entityfilepath);
-			
+
 			XStream xStream = new XStream(new DomDriver());
 			xStream.registerConverter(new EntityConverter());
 			String xmlstring = xStream.toXML(entity);
@@ -85,7 +84,8 @@ public class GameSaver {
 		return entityfilepath;
 	}
 
-	public void saveEntityImage(Entity entity, String filepath) {
+	public void saveEntityImage(Entity entity, String filepath)
+	{
 		try {
 
 			String sourcepathstring = entity.getImagePath();
