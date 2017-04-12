3113cf7f584295a806750f1c83dab834b40a23ba
Kyle Finke <Kyle Finke>
2017-04-12 17:17:53 -0400

    Fixed Collision Bug

--- src/engine/events/CollisionEvent.java
+++ src/engine/events/CollisionEvent.java
@@ -4,6 +4,7 @@ import engine.Event;
 import engine.Parameter;
 import engine.Collision;
 import engine.CollisionEventInterface;
+import engine.CollisionSide;
 
 public class CollisionEvent extends Event implements CollisionEventInterface {
 
@@ -11,6 +12,7 @@ public class CollisionEvent extends Event implements CollisionEventInterface {
 
 	public CollisionEvent() {
 		addParam(new Parameter("Entity", String.class, ""));
+		this.collision = new Collision(null, null, CollisionSide.ALL);
 	}
 
 	public void setCollision(Collision collision) {
@@ -20,11 +22,13 @@ public class CollisionEvent extends Event implements CollisionEventInterface {
 	@Override
 	public boolean act() {
 		for (Collision collision : getGameInfo().getObservableBundle().getCollisionObservable().getCollisions()) {
+			System.out.println("Collision" + collision);
+			System.out.println("This Collision" + this.collision);
 			if (collision.isBetween(getEntity().getName(), (String) getParam("Entity"))
 					&& collision.getCollisionSide().equals(this.collision.getCollisionSide()))
 				return true;
 		}
 		return false;
 	}
-	
+
 }
