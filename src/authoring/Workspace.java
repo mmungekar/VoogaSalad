ec4035042fbf255dcf17e4a39397d0d5a201bffb
Jimmy Shackford <jas199@duke.edu>
2017-04-12 20:41:02 -0400

    Fixed loading games with multiple layers.

--- src/authoring/Workspace.java
+++ src/authoring/Workspace.java
@@ -103,7 +103,7 @@ public class Workspace extends View
 		levelEditor.loadGame(game.getLevels());
 		defaults.setEntities(game.getDefaults());
 		panel.getSettings().load(game);
-		this.selectExistingLevel(1);
+		this.selectExistingLevel(levelEditor.getCurrentLevel().getLayerCount());
 		/*
 		 * game = new Game(); Level level = new Level(); Level level2 = new
 		 * Level(); Entity one = new CharacterEntity(); Entity two = new
