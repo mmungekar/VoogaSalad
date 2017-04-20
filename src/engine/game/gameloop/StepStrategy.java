0f4bef98d9a3582697be5eb14ec444b672bb2ae0
Nikita Zemlevskiy <naz7@duke.edu>
2017-04-20 11:24:44 -0400

    added SpawnEntityAction. modified gameInfo to have a reference to levelManager. modified CollisionSide to enable placing of entities based on side

--- src/engine/game/gameloop/StepStrategy.java
+++ src/engine/game/gameloop/StepStrategy.java
@@ -1,26 +1,27 @@
 package engine.game.gameloop;
 
 import engine.GameInfo;
-import engine.game.LevelManager;
 import engine.graphics.GraphicsEngine;
 import javafx.scene.Scene;
 
 /**
- * The interface for the Strategy Design Pattern whose subclasses can be substituted for 
- * different implementations of Screen's step().
+ * The interface for the Strategy Design Pattern whose subclasses can be
+ * substituted for different implementations of Screen's step().
+ * 
  * @author Matthew Barbano
  *
  */
 
 public interface StepStrategy {
 	/**
-	 * Called from Screen's constructor. Safest to put preliminary setup code rather than
-	 * subclass's constructor since do not know when it is instantiated, but more predictable
-	 * when Screen's constructor is called. 
-	 * @param info 
+	 * Called from Screen's constructor. Safest to put preliminary setup code
+	 * rather than subclass's constructor since do not know when it is
+	 * instantiated, but more predictable when Screen's constructor is called.
+	 * 
+	 * @param info
 	 */
-	public void setup(LevelManager levelManager, Scene gameScene, Screen screen, GraphicsEngine graphicsEngine, GameInfo info);
-	
+	public void setup(Scene gameScene, Screen screen, GraphicsEngine graphicsEngine, GameInfo info);
+
 	/**
 	 * Called from Screen's step() method on every iteration of the Timeline.
 	 */
