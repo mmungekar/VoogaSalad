d8053ec992a789991d7265b52f6ac41968ee9f1b
Nikita Zemlevskiy <naz7@duke.edu>
2017-04-20 14:56:57 -0400

    working on displaying new entities during gameplay

--- src/engine/actions/SpawnEntityAction.java
+++ src/engine/actions/SpawnEntityAction.java
@@ -8,6 +8,7 @@ import engine.Entity;
 import engine.Event;
 import engine.Parameter;
 import engine.events.AlwaysEvent;
+import javafx.application.Platform;
 
 public class SpawnEntityAction extends Action {
 
@@ -21,30 +22,40 @@ public class SpawnEntityAction extends Action {
 
 	@Override
 	public void act() {
-		Entity newEntity = null;
-		for (Entity entity : getGameInfo().getLevelManager().getCurrentLevel().getEntities()) {
-			if (((String) getParam("Entity Name")).equals(entity.getName()))
-				newEntity = entity.clone();
-		}
-		newEntity.setX(Double.parseDouble((String) getParam("X Speed")));
-		newEntity.setY(Double.parseDouble((String)getParam("Y Speed")));
-		String side = ((String) getParam("Side"));
-		CollisionSide collisionSide = getCollisionSide(side.toUpperCase());
-		collisionSide.placeEntity(getEntity(), newEntity);
-		if (newEntity.getXSpeed() != 0 || newEntity.getYSpeed() != 0) {
-			Event event = new AlwaysEvent();
-			event.addAction(new MoveAction());
-			newEntity.addEvent(event);
-		}
-		newEntity.getGameInfo().getLevelManager().getCurrentLevel().addEntity(newEntity);
+		System.out.println("INSIDE ACT");
+		Platform.runLater(new Runnable(){
+			@Override
+			public void run(){
+				System.out.println("INSIDE RUN");
+				Entity newEntity = null;
+				for (Entity entity : getGameInfo().getLevelManager().getCurrentLevel().getEntities()) {
+					if (((String) getParam("Entity Name")).equals(entity.getName()))
+						newEntity = entity.clone();
+				}
+				newEntity.setX((Double) getParam("X Speed"));
+				newEntity.setY((Double) getParam("Y Speed"));
+				String side = ((String) getParam("Side"));
+				CollisionSide collisionSide = getCollisionSide(side.toUpperCase());
+				collisionSide.placeEntity(getEntity(), newEntity);
+				if (newEntity.getXSpeed() != 0 || newEntity.getYSpeed() != 0) {
+					Event event = new AlwaysEvent();
+					event.addAction(new MoveAction());
+					newEntity.addEvent(event);
+				}
+				newEntity.getGameInfo().getLevelManager().getCurrentLevel().addEntity(newEntity);
+			}
+		});
+//		newEntity.getGameInfo().getLevelManager().getCurrentLevel().addEntity(newEntity);
 	}
 
 	private CollisionSide getCollisionSide(String side) {
 		try {
-			Class<?> clazz = Class.forName("engine.CollisionSide." + side);
-			Constructor<?> ctor = clazz.getDeclaredConstructor();
-			return (CollisionSide) ctor.newInstance();
+			//Class<?> clazz = Class.forName("engine.CollisionSide." + side);
+			CollisionSide collisionSide = CollisionSide.valueOf(side); 
+			//Constructor<?> ctor = clazz.getDeclaredConstructor();
+			return collisionSide;
 		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
