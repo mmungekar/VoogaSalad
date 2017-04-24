ebf52a49efbbe917e984dce6dee12612f528be92
Mina Mungekar <mina.mungekar@duke.edu>
2017-04-23 20:43:13 -0400

    AchievementEvent

---
+++ src/engine/events/AchievementEvent.java
@@ -0,0 +1,17 @@
+package engine.events;
+
+import engine.Event;
+
+public class AchievementEvent extends Event {
+	
+	public AchievementEvent(){
+		
+	}
+
+	@Override
+	public boolean act() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
