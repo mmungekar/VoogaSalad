6ed40106cf51be20c57bb822311a0bc46f24a95d
Mina Mungekar <mina.mungekar@duke.edu>
2017-04-28 00:42:55 -0400

    Timer tweaking

--- src/authoring/components/CustomTooltip.java
+++ src/authoring/components/CustomTooltip.java
@@ -16,6 +16,7 @@ public class CustomTooltip extends Tooltip {
 	}
 	
 	public CustomTooltip(StringBinding string){
+		super();
 		this.textProperty().bind(string);
 		this.setMaxWidth(200);
 		this.setWrapText(true);
@@ -25,17 +26,18 @@ public class CustomTooltip extends Tooltip {
 	//Resource used: http://stackoverflow.com/questions/26854301/control-javafx-tooltip-delay
 	private void shortenToolTipTime() {
 	    try {
-	        Field fieldBehavior = this.getClass().getDeclaredField("BEHAVIOR");
+	        Field fieldBehavior = this.getClass().getSuperclass().getDeclaredField("BEHAVIOR");
 	        fieldBehavior.setAccessible(true);
 	        Object objBehavior = fieldBehavior.get(this);
 
-	        Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
+	        Field fieldTimer = objBehavior.getClass().getSuperclass().getDeclaredField("activationTimer");
 	        fieldTimer.setAccessible(true);
 	        Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
 
 	        objTimer.getKeyFrames().clear();
 	        objTimer.getKeyFrames().add(new KeyFrame(new Duration(250)));
 	    } catch (Exception e) {
+	    	System.out.println("not working");
 
 	    }
 	}
