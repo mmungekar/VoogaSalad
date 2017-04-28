a84d52b74b4e6ee343a5964366794a6aa1a3b632
Kyle Finke <Kyle Finke>
2017-04-27 21:37:39 -0400

    Storing changes for merge

---
+++ src/engine/events/regular_events/InCameraRegionEvent.java
@@ -0,0 +1,17 @@
+package engine.events.regular_events;
+
+import engine.events.Event;
+
+public class InCameraRegionEvent extends Event {
+
+	public InCameraRegionEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean act() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
