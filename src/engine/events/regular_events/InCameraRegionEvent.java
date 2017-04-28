1fd33acb470743af53888d1038445b0c7f7928c5
Kyle Finke <Kyle Finke>
2017-04-27 21:53:16 -0400

    Added InCameraRegionEvent

--- src/engine/events/regular_events/InCameraRegionEvent.java
+++ src/engine/events/regular_events/InCameraRegionEvent.java
@@ -1,17 +1,25 @@
 package engine.events.regular_events;
 
+import engine.entities.entities.CameraEntity;
 import engine.events.Event;
 
+/**
+ * Checks to see if the Entity associated with this Event is within the region
+ * of the CameraEntity chosen for their game. act() returns true if the
+ * associated Entity is in the camera region and false otherwise.
+ * 
+ * @author Kyle Finke
+ *
+ */
 public class InCameraRegionEvent extends Event {
-
-	public InCameraRegionEvent() {
-		// TODO Auto-generated constructor stub
-	}
 
 	@Override
 	public boolean act() {
-		// TODO Auto-generated method stub
-		return false;
+		CameraEntity camera = getGameInfo().getGraphicsEngine().getCamera();
+		return !(getEntity().getX() > camera.getX() + camera.getWidth()
+				|| getEntity().getX() + getEntity().getWidth() < camera.getX()
+				|| getEntity().getY() > camera.getY() + camera.getHeight()
+				|| getEntity().getY() + getEntity().getHeight() < camera.getY());
 	}
 
 }
