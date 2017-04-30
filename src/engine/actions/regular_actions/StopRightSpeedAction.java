59257d3bd73da6f72bab4e134e1f518ec918da5d
Jay Doherty <jld60@duke.edu>
2017-04-30 18:15:36 -0400

    Added class comments.

--- src/engine/actions/regular_actions/StopRightSpeedAction.java
+++ src/engine/actions/regular_actions/StopRightSpeedAction.java
@@ -3,9 +3,9 @@ package engine.actions.regular_actions;
 import engine.actions.Action;
 
 /**
- * Sets the horizontal speed of the corresponding Entity to zero.
+ * Sets the horizontal speed of the corresponding Entity to zero if it is moving right.
  * 
- * @author Kyle Finke
+ * @author Jay Doherty
  *
  */
 public class StopRightSpeedAction extends Action {
--- src/engine/events/regular_events/EntityIsVisibleEvent.java
+++ src/engine/events/regular_events/EntityIsVisibleEvent.java
@@ -6,6 +6,13 @@ import engine.Parameter;
 import engine.entities.Entity;
 import engine.events.Event;
 
+/**
+ * An Event that triggers every frame (like an AlwaysEvent) as long as the specified Entity is
+ * visible. This could provide different behavior for if an Entity is hiding vs. visible.
+ * 
+ * @author Jay Doherty
+ *
+ */
 public class EntityIsVisibleEvent extends Event {
 
 	public EntityIsVisibleEvent() {
