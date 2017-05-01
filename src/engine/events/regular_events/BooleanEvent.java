7e0957547d8e9904d386d6007888e3f6224bff79
Nikita Zemlevskiy <naz7@duke.edu>
2017-05-01 01:02:18 -0400

    add boolean event

--- src/engine/events/regular_events/BooleanEvent.java
+++ src/engine/events/regular_events/BooleanEvent.java
@@ -5,13 +5,18 @@ import engine.events.Event;
 
 public class BooleanEvent extends Event {
 	private BooleanParser parser;
-	
-	public BooleanEvent(){		
+
+	public BooleanEvent() {
 		addParam(new Parameter(getResource("Expression"), String.class, ""));
 		parser = new BooleanParser();
 	}
+
 	@Override
 	public boolean act() {
-		return parser.parse((String)getParam(getResource("Expression")));
+		try {
+			return parser.parse((String) getParam(getResource("Expression")));
+		} catch (Exception e) {
+
+		}
 	}
 }
