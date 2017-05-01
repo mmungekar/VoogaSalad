142d9d298485b99448572c28caf5b6b2a150665b
Elliott Bolzan <elliott.bolzan@duke.edu>
2017-05-01 01:02:52 -0400

    Added comments to BooleanParser.

--- src/boolean_parser/BooleanParser.java
+++ src/boolean_parser/BooleanParser.java
@@ -5,15 +5,28 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 /**
+ * 
+ * Boolean parsing adapted from:
+ * http://stackoverflow.com/questions/19383953/is-it-possible-to-evaluate-a-boolean-expression-for-string-comparions.
+ * 
+ * Uses the default JavaScript engine to parse a boolean expression. In the
+ * event that the expression is improperly formatted, the parser returns false.
+ * This is by design: no Event will be triggered if the expression is incorrect,
+ * which is the expected behavior.
+ * 
  * @author Elliott Bolzan
  *
  */
 public class BooleanParser {
 
-	public Boolean parse(String expression) throws ScriptException {
-		ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("JavaScript");
-        return (Boolean) engine.eval(expression);
+	public Boolean parse(String expression) {
+		try {
+			ScriptEngineManager manager = new ScriptEngineManager();
+			ScriptEngine engine = manager.getEngineByName("JavaScript");
+			return (Boolean) engine.eval(expression);
+		} catch (ScriptException e) {
+			return false;
+		}
 	}
 
 }
