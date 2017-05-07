745352d21dfecc1d90c6517294b56a7249a96114
Matthew Barbano <matthew.barbano@duke.edu>
2017-05-07 00:11:31 -0400

    Continued adding Javadoc
    Former-commit-id: 0ee9cd72274a8f85dc19e89fb82f89e5d792a9c8

--- src/engine/game/selectiongroup/Selectable.java
+++ src/engine/game/selectiongroup/Selectable.java
@@ -1,6 +1,8 @@
 package engine.game.selectiongroup;
 
 /**
+ * Specifies an object that is allowed to be a member of a SelectionGroup.
+ * (intentionally left blank)
  * 
  * @author Matthew Barbano
  *
